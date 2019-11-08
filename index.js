04a17c32a22fd65d86a24dd403cc4b30b8195648
Sam <Jeffcoat>
2019-11-07 18:09:02 -0600

    Copied over and caught up

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 {
     type: 'ADD_TODO',
         todo: {
-        id: 0,
-            name: "Learn Redux",
+    id: 0,
+        name: 'Learn Redux',
         complete: false,
-    }
-
 }
+}
+
 {
     type: 'REMOVE_TODO',
         id: 0,
@@ -14,30 +14,44 @@
 
 {
     type: 'TOGGLE_TODO',
-        id: 0
+        id: 0,
 }
 
 {
     type: 'ADD_GOAL',
         goal: {
-        id:0,
-            name: 'Run a Marathon'
+    id: 0,
+        name: 'Run a Marathon'
 }
 }
 
 {
     type: 'REMOVE_GOAL',
-    id: 0
-
+        id: 0
 }
 
+/*
+Characteristics of a Pure Function
+1) They always return the same result if the same arguments are passed in.
+2) They depend only on the arguments passed into them.
+3) Never produce any side effects.
+*/
 
-function createStore (){
-    // store should have four parts
+// Reducer function
+function todos (state = [], action) {
+    if (action.type === 'ADD_TODO') {
+        return state.concat([action.todo])
+    }
+
+    return state
+}
+
+function createStore (reducer) {
+    // The store should have four parts
     // 1. The state
-    // 2. Get the state
-    // 3. Listen to changes on the state
-    // 4. update the state
+    // 2. Get the state. (getState)
+    // 3. Listen to changes on the state. (subscribe)
+    // 4. Update the state (dispatch)
 
     let state
     let listeners = []
@@ -47,13 +61,20 @@ function createStore (){
     const subscribe = (listener) => {
         listeners.push(listener)
         return () => {
-            listeners = listerners.filter((l)=> l !== listener )
+            listeners = listeners.filter((l) => l !== listener)
         }
+    }
 
+    const dispatch = (action) => {
+        state = reducer(state, action)
+        listeners.forEach((listener) => listener())
     }
 
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch,
     }
 }
+
+const store = createStore(todos)
